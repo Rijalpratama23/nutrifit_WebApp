5b818cef6cf06e175d4ec2fa56d0b8059f62c657
Rijalpratama23 <rijal.pratama_ti23@gmail.com>
2026-06-13 09:14:43 +0700

    all fiture dashboard admin done

--- utils/supabase/admin.ts
+++ utils/supabase/admin.ts
@@ -3,3 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 export const createAdminClient = () => {
   return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
 };
+
+// Tambah baris ini:
+export const supabaseAdmin = createAdminClient();
