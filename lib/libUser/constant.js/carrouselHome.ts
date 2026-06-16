5d662f036461e19d8e07473262db24b8a4261631
Rijalpratama23 <rijal.pratama_ti23@gmail.com>
2026-06-16 15:14:07 +0700

    done button use Props

--- lib/libUser/constant.js/carrouselHome.ts
+++ lib/libUser/constant.js/carrouselHome.ts
@@ -1,21 +1,26 @@
-// lib/constants.js
 export const HERO_SLIDES = [
   {
     id: 1,
     title: 'Konsultasi Sekarang!',
     desc: 'Panduan Personal ahli gizi dan sesuai data dan tujuan pengguna',
     img: '/landingPage/hero.png',
+    buttonText: 'Konsultasi Sekarang!',
+    buttonDestination: '/user/konsultasi',
   },
   {
     id: 2,
     title: 'Program Nutrisi',
     desc: 'Ikuti program kesehatan yang dirancang khusus untuk Anda',
     img: '/landingPage/hero.png',
+    buttonText: 'Lihat Program',
+    buttonDestination: '/user/program',
   },
   {
     id: 3,
     title: 'Rencana Makan',
     desc: 'Atur jadwal makan harianmu dengan lebih teratur',
     img: '/landingPage/hero.png',
+    buttonText: 'Atur Rencana Makan',
+    buttonDestination: '/user/rencanaNutrisi',
   },
 ];
