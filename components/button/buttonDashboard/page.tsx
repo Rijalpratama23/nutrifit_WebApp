d48e71ebd7a9411945e2ebd3901bc6e8ee341074
Rijalpratama23 <rijal.pratama_ti23@gmail.com>
2026-06-16 14:34:41 +0700

    masih masalah button

--- components/button/buttonDashboard/page.tsx
+++ components/button/buttonDashboard/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 interface ButtonDashboardProps {
   text: string;
@@ -12,8 +12,8 @@ export default function ButtonDashboard({ text, background, destination }: Butto
   const router = useRouter();
 
   return (
-    <button onClick={() => router.push(destination)} className={`${background} cursor-pointer text-white py-2 px-10 rounded-lg active:scale-95 transition-all duration-200`}>
-      {text}
-    </button>
+    <Link href={destination}>
+      <button className={`${background} cursor-pointer text-white py-2 px-10 rounded-lg active:scale-95 transition-all duration-200`}>{text}</button>
+    </Link>
   );
 }
